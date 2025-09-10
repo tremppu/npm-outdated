a827714be24a57223934533b57e50fd7d4069777
teemukissa <tremppu@hotmail.com>
2025-09-10 09:17:18 +0300

    feat: add "safe to update" check

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { exec } from "child_process";
 
-exec("npm outdated --json", (error, stdout, stderr) => {
+exec("npm outdated --json", async (error, stdout, stderr) => {
   const data = JSON.parse(stdout);
 
   const outdated = Object.entries(data).reduce((acc, [name, value]) => {
@@ -11,9 +11,27 @@ exec("npm outdated --json", (error, stdout, stderr) => {
     return acc;
   }, []);
 
-  const wanted = outdated
-    .map(([name, version]) => `${name}@${version}`)
-    .join(" ");
+  const now = new Date();
 
-  console.log(wanted);
+  const safeToUpdate = [];
+  const unsafeToUpdate = [];
+
+  for (const [name, version] of outdated) {
+    const metadata = await fetch(`https://registry.npmjs.org/${name}`).then(
+      (res) => res.json()
+    );
+    const versionTimestamp = metadata.time[version];
+    const versionDate = new Date(versionTimestamp);
+    const diffTime = Math.abs(now - versionDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    // If package is at least 2 days old, consider it safe to update
+    if (diffDays >= 2) safeToUpdate.push(`${name}@${version}`);
+    else unsafeToUpdate.push(`${name}@${version}`);
+  }
+
+  console.log(
+    `Found ${outdated.length} outdated packages from which ${unsafeToUpdate.length} are unsafe to update at this point. Safe to update:`
+  );
+  console.log(safeToUpdate.join(" "));
 });
